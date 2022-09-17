8a4fb469f63e8744026faeb6dce6d53aee8c21af
EvanCasey13 <evancasey2003@gmail.com>
2022-09-17 16:32:58 +0100

    Inital Setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import GameHomepage from './Pages/Games/GameHomepage';
+import ForumHomepage from './Pages/Forum/ForumHomepage';
+import About from './Pages/About/About';
+import Layout from './Pages/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<header className="App-header">
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="gameHomepage" element={<GameHomepage />} />
+          <Route path="forumHomepage" element={<ForumHomepage />} />
+          <Route path="about" element={<About />} />
+        </Route>
+    </Routes>
+  </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/Pages/Games/GameHomepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GameHomepage() {
+
+    return (
+       <h1>This is the Game Homepage</h1>
+    );
+
+}
+
+export default GameHomepage;
