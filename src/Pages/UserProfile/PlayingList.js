287e0e14324cc341919ad2c76f2b1b5250955fd5
EvanCasey13 <evancasey2003@gmail.com>
2022-11-17 21:55:23 +0000

    Added functionality to get favourite games from firestore.

--- src/Pages/UserProfile/PlayingList.js
+++ src/Pages/UserProfile/PlayingList.js
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { auth, db } from "../../Firebase/firebase";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth"
+import Game from "../../Components/Game/Game"
 
 const PlayingList = () => {
-    const getList = () => {
-        onAuthStateChanged(auth, (user) => {      
-            const snapRef2 = collection(db, "users/" + user.uid + "/favourites")
-            onSnapshot(snapRef2, (snapshot) => {
-                const favourites = []
-                snapshot.forEach((doc) => {
-                    favourites.push(doc.data())
-                })
-                console.log(favourites)
-            })
-            });
-       }
+
+const [favourites, setFavourites] = useState([])
+
+useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        let getFavourites = await getDocs(collection(db, "users/" + user.uid + "/favourites"))
+        setFavourites(getFavourites.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id
+        })))
+      }
+    });
+  }, [])
        
     return (
-        getList()
+  favourites.map(fav => {
+    console.log(favourites)
+    return (
+        <Game 
+        key={fav.game.id} game={fav.game}/>
+    )
+  })
     )
 };
 
