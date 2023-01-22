b2e0307cfefe72084d405c8670f7705109fab1dc
EvanCasey13 <evancasey2003@gmail.com>
2023-01-22 17:02:17 +0000

    Favourites feature - broke functionality into more componenets

--- src/Pages/UserProfile/OnHoldList.js
+++ src/Pages/UserProfile/OnHoldList.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import { auth, db } from "../../Firebase/firebase";
 import { collection, getDocs, doc, query, where } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { onAuthStateChanged } from "firebase/auth"
-import Game from "../../Components/FavouriteGame/FavouriteGame"
 import './UserProfile.css'
 import NavBar from "../../Components/Navbar/Navbar"
-import { Grid } from "@nextui-org/react";
+import FavouritesList from "../../Components/FavouriteListPage/FavouriteListPage";
 
 const OnHoldList = () => {
 
@@ -16,9 +14,9 @@ const OnHoldList = () => {
   useEffect(() => {
     const playingRef = collection(db, "users/" + user?.uid + "/favourites");
     const q = query(playingRef, where("Status", "==", "On-hold"));
-    const getFavourites = async () =>{
+    const getFavourites = async () => {
       const data = await getDocs(q)
-      setFavourites(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      setFavourites(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
     }
     getFavourites()
   }, [user?.uid]);
@@ -26,16 +24,7 @@ const OnHoldList = () => {
   return (
     <div className="OnHoldList">
       <NavBar />
-      <Grid.Container gap={1}>
-        {favourites.map(fav => {
-          return (
-            <Grid xs={12} sm={6} md={6} lg={6} xl={6} key={fav.game.id}>
-              <Game
-                key={fav.game.id} game={fav.game} rating={fav.Rating} status={fav.Status} id={fav.game.id} />
-            </Grid>
-          )
-        })}
-      </Grid.Container>
+      <FavouritesList favourites={favourites} />
     </div>
   )
 };
