da4c0d44af5c1377f2010893923190a9c0614247
EvanCasey13 <evancasey2003@gmail.com>
2023-01-26 17:33:55 +0000

    NextUI - Added relevant icons to forum sections

---
+++ src/Icons/SupportIcon.js
@@ -0,0 +1,81 @@
+import * as React from "react";
+const SupportIcon = (props) => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 32 32"
+    enableBackground="new 0 0 32 32"
+    id="Stock_cut"
+    xmlSpace="preserve"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <desc />
+    <g>
+      <path
+        d="M7,25H6   c-2.761,0-5-2.239-5-5v0c0-2.761,2.239-5,5-5h1V25z"
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+      <path
+        d="M26,25h-1V15h1   c2.761,0,5,2.239,5,5v0C31,22.761,28.761,25,26,25z"
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+      <path
+        d="M5,15v-3   C5,5.925,9.925,1,16,1h0c6.075,0,11,4.925,11,11v3"
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+      <path
+        d="M27,25v4   c0,1.105-0.895,2-2,2h-3"
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+      <line
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+        x1={18}
+        x2={20}
+        y1={31}
+        y2={31}
+      />
+      <line
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+        x1={16}
+        x2={16}
+        y1={24}
+        y2={26}
+      />
+      <path
+        d="M16,22v-2.615   c0-1.441,0.877-2.736,2.215-3.271l0,0C19.897,15.441,21,13.812,21,12v0c0-2.761-2.239-5-5-5h0c-2.761,0-5,2.239-5,5v0"
+        fill="none"
+        stroke="#000000"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+    </g>
+  </svg>
+);
+export default SupportIcon;
